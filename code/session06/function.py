2a0d4c58c6854e2bc48fe6d60f386990152ad76c
Bruce.Song <bruce.song@cafx.com>
2023-11-18 17:30:26 +1100

    with shooter

---
+++ code/session06/function.py
@@ -0,0 +1,5 @@
+def square(number):
+  return number * number
+
+num = int(input('Please input a number:'))
+print(f"The square number for {num} is {square(num)}")
---
+++ code/shooter/shooter.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Air shooter')
+clock = pygame.time.Clock()
+background = pygame.image.load('resources/image/background.png').convert()
+plane_img = pygame.image.load('resources/image/shoot.png').convert_alpha()
+plane_rect = pygame.Rect(0, 99, 102, 126)
+img = plane_img.subsurface(plane_rect).convert_alpha()
+plane_rect.topleft = [200, 400]
+running = True
+speed = 5
+while running:
+  clock.tick(45)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+  key_pressed = pygame.key.get_pressed()
+  if key_pressed[pygame.K_UP]:
+    plane_rect.top -= speed
+    if plane_rect.top < 0:
+      plane_rect.top = 0
+  elif key_pressed[pygame.K_DOWN]:
+    plane_rect.top += speed
+    if plane_rect.top > SCREEN_HEIGHT - plane_rect.height:
+      plane_rect.top = SCREEN_HEIGHT - plane_rect.height
+      
+  screen.fill(0)
+  screen.blit(background, (0,0))
+  screen.blit(img, plane_rect)
+  pygame.display.update()
